59ca4a4bf7c35d9a522592efbc7e8852e505dc82
alalelu <zekrezvyj@gmail.com>
2024-03-23 15:15:37 +0300

    create class Player

--- main.py
+++ main.py
@@ -14,6 +14,20 @@ class GameSprite(pygame.sprite.Sprite):
         self.rect.y = y
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_left(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s] and self.rect.y + self.rect.height < 500:
+            self.rect.y += self.speed
+    def update_right(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN] and self.rect.y + self.rect.height < 500:
+            self.rect.y += self.speed
 while True:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
